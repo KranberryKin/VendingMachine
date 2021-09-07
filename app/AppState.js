0a6bc2bf6def8732a684f064223cf9789e58a405
Quinnton <quinnclimbs@gmail.com>
2021-09-07 10:15:51 -0600

    commit

--- app/AppState.js
+++ app/AppState.js
@@ -10,6 +10,12 @@ class AppState extends EventEmitter {
       price: 1.25,
       quantity: 30,
       imgUrl: 'https://images.unsplash.com/photo-1531384370597-8590413be50a?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=334&q=80',
+    }),
+    new Item({
+      name: 'Coke',
+      price: 1.25,
+      quantity: 25,
+      imgUrl: "https://images.unsplash.com/photo-1554866585-cd94860890b7?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=401&q=80"
     })
   ]
   totalMoney = 0
